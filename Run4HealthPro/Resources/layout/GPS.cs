b4801b073c68cdb5edf9d16ab62c2206045f83f8
Jakub Zalubski <warrior12539@gmail.com>
2023-12-05 08:59:31 +0100

    Utworzenie pliku GPS.cs

--- Run4HealthPro/Resources/layout/GPS.cs
+++ Run4HealthPro/Resources/layout/GPS.cs
@@ -1,17 +1,33 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
 
 namespace Run4HealthPro.Resources.layout
 {
-    class GPS
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Gps : ContentPage
     {
+        public Gps()
+        {
+            InitializeComponent();
+        }
+        /// IGeolocator locator = DependencyService.Get<IGeolocator>(); //lub var locator jak poniżej  
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10000), null, true);
+            LongitudeLabel.Text = position.Longitude.ToString();
+            LatitudeLabel.Text = position.Latitude.ToString();
+        }
     }
 }
